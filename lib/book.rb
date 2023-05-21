c25c94aa491ad9b1e85f073adf3ee40c36803119
joseph <josephmacharia575@gmail.com>
2023-05-21 21:33:42 +0300

    complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,31 @@
+class Book 
+    attr_reader :title
+    attr_reader :author
+    attr_reader :page_count
+    attr_reader :genre
+    attr_reader :turn_page
+
+    def initialize title 
+        @title = title
+       #
+    end 
+    def author= (author_name )
+        @author= author_name
+    end 
+
+    def page_count= (page_count)
+
+        @page_count = page_count
+    end 
+    def genre= (genre)
+        @genre =genre
+    end 
+
+    def turn_page
+
+        puts "Flipping the page...wow, you read fast!"
+    end 
+end 
+Book.new ("And Then There Were None")
 
 
